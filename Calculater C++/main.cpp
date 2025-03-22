7ca12611acf7772187a87e0252fe4fb263022d5a
Akila Induwara Kaluarachchi <k.a.akilainduwara@gmail.com>
2025-03-22 16:25:46 +0530

    C++

--- Calculater C++/main.cpp
+++ Calculater C++/main.cpp
@@ -6,6 +6,24 @@ int first =0;
 int second =0;
 int sw =0;
 
+
+int Addition(int first, int second) {
+    return first + second;
+}
+int Subtraction(int first, int second) {
+    return first - second;
+}
+int Multiplication(int first, int second) {
+    return first * second;
+}
+int Division(int first, int second) {
+    return first / second;
+}
+int Modulus(int first ,int second) {
+    return first % second;
+}
+
+
 int main(){
 
     cout<<"Welcome \n ";
@@ -15,22 +33,37 @@ int main(){
     cout<<"Enter the second number: ";
     cin >> second;
 
-    cout<<"Enter the Number for Proceess \n";
+    cout<<"01.Addition \n"
+          "02.Subtraction \n"
+          "03.Multiplication \n"
+          "04.Division \n"
+          "05.Modulus \n";
 
+    cout<<"Enter the Number for Proceess \n";
+    cin>>sw;
 
     switch(sw) {
-        case 1:
-            cout<<sw<<"Addition\n";
-        case 2:
-            cout<<sw<<"Subtraction\n ";
-        case 3:
-            cout<<sw<<"Multiplication\n ";
-        case 4:
-            cout<<sw<<"Division\n ";
-        case 5:
-            cout<<sw<<"Modulus\n ";
-
-
+        case 1: {
+            cout<<"Addition = "<<Addition(first,second);
+            break;
+        }
+        case 2: {
+            cout<<"Subtraction = "<<Subtraction(first,second);
+            break;
+        }
+        case 3: {
+            cout<<"Multiplication = "<<Multiplication(first,second);
+            break;
+        }
+        case 4: {
+            cout<<"Division = "<<Division(first,second);
+            break;
+        }
+        case 5: {
+            cout<<"Modulus = "<<Modulus(first,second);
+            break;
+        }
 
     }
 }
+
