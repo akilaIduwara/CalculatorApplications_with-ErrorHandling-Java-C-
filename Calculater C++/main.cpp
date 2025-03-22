2244d819fd04614c51003f16d6507179c88a5f25
Akila Induwara Kaluarachchi <k.a.akilainduwara@gmail.com>
2025-03-22 15:41:57 +0530

    Seperate Java and C++ Folder

---
+++ Calculater C++/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<iostream>
+using namespace std;
+
+int first =0;
+int second =0;
+int sw =0;
+
+int main(){
+
+    cout<<"Welcome \n ";
+    //Getiing input
+    cout<<"Enter the first number: ";
+    cin >> first;
+    cout<<"Enter the second number: ";
+    cin >> second;
+
+    cout<<"Enter the Number for Proceess \n";
+
+
+    switch(sw) {
+        case 1:
+            cout<<sw<<"Addition\n";
+        case 2:
+            cout<<sw<<"Subtraction\n ";
+        case 3:
+            cout<<sw<<"Multiplication\n ";
+        case 4:
+            cout<<sw<<"Division\n ";
+        case 5:
+            cout<<sw<<"Modulus\n ";
+
+
+
+    }
+}
